3cca92343e46754eb88690cac02edb15914bd300
MaddePorserud <madelen.porserud@gmail.com>
2016-09-08 11:51:28 +0200

    lista med testdata

--- quiz/views.py
+++ quiz/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
+
 
 quizzes = [
 {"quiz_number":1,
@@ -15,7 +15,10 @@ quizzes = [
 "description":"HAckerhistoria är viktig, kan du den?"},
 ]
 def startpage (request):
-	return render(request, "quiz/start.html")
+	context = {
+	"quizzes":quizzes,
+	}
+	return render(request, "quiz/start.html", context)
 def quiz(request):
 	return render(request, "quiz/quiz.html")
 def question(request):
