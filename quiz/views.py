fdd51d5dbe3c788a4320a4c7911b0e7894a59caf
MaddePorserud <madelen.porserud@gmail.com>
2016-09-08 11:39:06 +0200

    Skapat sidorna så att de finns på webbservern

--- quiz/views.py
+++ quiz/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
-	def startpage (request):
-		return render(request, "quiz/start.html")
-	def quiz(request):
-		return render(request, "quiz/quiz.html")
-	def question(request):
-		return render(request, "quiz/question.html")
-	def completed(request):
-		return render(request, "quiz/results.html")
+def startpage (request):
+	return render(request, "quiz/start.html")
+def quiz(request):
+	return render(request, "quiz/quiz.html")
+def question(request):
+	return render(request, "quiz/question.html")
+def completed(request):
+	return render(request, "quiz/results.html")
