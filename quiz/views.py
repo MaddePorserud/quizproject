46baeaca9b78be82cf7b37f9dbc3a7f7a2c67f06
MaddePorserud <madelen.porserud@gmail.com>
2016-09-08 11:08:38 +0200

    created views for the pages

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+	def startpage (request):
+		return render(request, "quiz/start.html")
+	def quiz(request):
+		return render(request, "quiz/quiz.html")
+	def question(request):
+		return render(request, "quiz/question.html")
+	def completed(request):
+		return render(request, "quiz/results.html")
