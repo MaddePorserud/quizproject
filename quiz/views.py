13ea2b8d016638cca0db0924a7c300a2e69d40cd
MaddePorserud <madelen.porserud@gmail.com>
2016-09-09 10:59:18 +0200

    Skapat URL och view för användarens svar på frågorna

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from django.shortcuts import render
 
 # Create your views here.
@@ -21,8 +22,8 @@ def question(request, quiz_number, question_number):
 	questions = quiz.questions.all()
 	question = questions[int(question_number)-1]
 	context = {
-	"question_number":number,
-	"question":question.question
+	"question_number":question_number,
+	"question":question.question,
 	"answer1":question.answer1,
 	"answer2":question.answer2,
 	"answer3":question.answer3,
@@ -30,6 +31,15 @@ def question(request, quiz_number, question_number):
 	"quiz_number":quiz_number,
 	}
 	return render(request, "quiz/question.html",context)
+def answer(request, quiz_numver, question_number):
+	saved_answers = request.session.get(quiz_number,{})
+	answer = int(request.POST["answer"])
+	saved_answers[question_number] = answer
+	request.session[quiz_number] = saved_answers
+
+	question_number=int(question_number)
+	return redirect("question_page", quiz_number, question_number + 1)
+
 def completed(request, quiz_number):
 	context = {
 	"correct":12,
