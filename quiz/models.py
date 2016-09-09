c15efa6e909da0a9e14dbcf005259135e0e39a85
MaddePorserud <madelen.porserud@gmail.com>
2016-09-09 10:13:00 +0200

    Har skapat två modeller och migrerat dem till Django
    Med hjälp av migrationen har en databastabell skapats i DJango

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+#models.Model anropar klasser i Django.
+class Quiz(models.Model):
+		quiz_number = models.PositiveIntegerField()
+		name = models.CharField(max_length=100)
+		description = models.TextField()
+		# Anger här information om vad de olika objekten i klassen kommer
+		# innehålla. GEnom att speca Integer (Siffra) lagras ett anpassat område för detta
+
+class Question(models.Model):
+	question = models.TextField()
+	answer1 = models.CharField(max_length=100)
+	answer2 = models.CharField(max_length=100)
+	answer3 = models.CharField(max_length=100)
+	correct = models.PositiveIntegerField()
+	quiz = models.ForeignKey(Quiz, related_name="questions")
